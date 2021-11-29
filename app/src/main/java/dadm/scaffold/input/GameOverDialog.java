666186bf53b5ec100162d20bbf40a7f8a8dd2517
MiguelVDP <miguelvidaldelaplaza@gmail.com>
2021-11-29 20:50:40 +0100

    Algo he cambiado (creo que lo del highscore)

--- app/src/main/java/dadm/scaffold/input/GameOverDialog.java
+++ app/src/main/java/dadm/scaffold/input/GameOverDialog.java
@@ -1,5 +1,7 @@
 package dadm.scaffold.input;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,7 +17,17 @@ public class GameOverDialog extends BaseCustomDialog implements View.OnClickList
         findViewById(R.id.btn_replay).setOnClickListener(this);
         findViewById(R.id.btn_exit).setOnClickListener(this);
         TextView txt = (TextView) findViewById(R.id.score_text);
-        txt.setText("Your score: " + score);
+        txt.setText("YOUR SCORE: " + score);
+        TextView hsTxt = (TextView) findViewById(R.id.high_score_text);
+        SharedPreferences preferences = activity.getSharedPreferences("score", Context.MODE_PRIVATE);
+        int highScore = preferences.getInt("highScore", 0);
+        if(score > highScore){
+            highScore = score;
+            SharedPreferences.Editor objEditor = preferences.edit();
+            objEditor.putInt("highScore", score);
+            objEditor.commit();
+        }
+        hsTxt.setText("HIGH SCORE: " + highScore);
     }
     public void setListener(GameOverDialogListener listener) {
         mListener = listener;
